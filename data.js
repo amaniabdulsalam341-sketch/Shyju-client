49ac6cd89646e41b2f124c8f7ba1d01f7295ecb3
amaniabdulsalam341-sketch <amaniabdulsalam341@gmail.com>
2025-11-24 05:42:25 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,105 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-master-bedroom",
+      "name": "Master bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.8376888030412015,
+        "pitch": 0.07839684749705711,
+        "fov": 1.510284771193917
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.14200629190874992,
+          "pitch": -0.06818591157662723,
+          "rotation": 0,
+          "target": "1-walk-in-wardrobe"
+        },
+        {
+          "yaw": -0.34737005555708045,
+          "pitch": -0.016434298996538033,
+          "rotation": 0,
+          "target": "2-office"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-walk-in-wardrobe",
+      "name": "Walk in wardrobe",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.021764032838353486,
+        "pitch": -0.01649969892756964,
+        "fov": 1.510284771193917
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-office",
+      "name": "Office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.510284771193917
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
